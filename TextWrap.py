0cf8f527ec0b50ea518b85555baf6b431867a07a
Meysiolio <saturninsky@gmail.com>
2021-09-23 10:50:38 +0330

    first commit

---
+++ TextWrap.py
@@ -0,0 +1,16 @@
+def wrap(string, max_width):
+    n = 0
+    l = []
+    while (n+1) * max_width <= len(string):
+        l.append(string[n*max_width:(n+1)*max_width])
+        n += 1
+    l.append(string[n*max_width:])
+    
+    string = '\n'.join(l)
+
+    return string
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
